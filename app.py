daf7b0b080f5eb694c38d9c005c5ee34f532af8e
Edb83 <ed3radley@gmail.com>
2020-12-29 18:50:47 +0000

    Add unique filter to activity_equipment to prevent duplicates; add categories to view_activity to display cat image if activity image is blank

--- app.py
+++ app.py
@@ -333,7 +333,8 @@ def delete_activity(activity_id):
 @app.route("/view_activity/<activity_id>")
 def view_activity(activity_id):
     activity = mongo.db.activities.find_one({"_id": ObjectId(activity_id)})
-    return render_template("view_activity.html", activity=activity)
+    categories = list(mongo.db.categories.find())
+    return render_template("view_activity.html", activity=activity, categories=categories)
 
 
 @app.route("/categories")
